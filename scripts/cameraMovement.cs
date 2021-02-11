578bf0983e7d1156771deccf7f082094820dfa46
bartmethond <b.hassink@live.com>
2021-02-11 17:46:16 +0100

    this contains the unity scripts

---
+++ scripts/cameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+    public float xPos;
+    public float posit;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-xPos, 0f, 0f) * moveSpeed * Time.deltaTime;
+        posit = transform.position.x;
+    }
+}
---
+++ scripts/warpScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class warpScript : MonoBehaviour
+{
+    public cameraMovement cameraMove;
+    public overSeeer overLord;
+    public float YScale;
+    public Light bright;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(overLord.timeDisplay);
+        Debug.Log("modulo 1 = " + overLord.timeDisplay % 1);
+        if(overLord.timeDisplay % 1 < 0.2)
+        {
+            YScale = 1;
+        }
+        else
+        {
+            YScale = 2;
+        }
+        float currentY = transform.localScale.y;
+        float percentage = overLord.timeDisplay % 1;
+        float lerpedY = Mathf.Lerp(currentY, YScale, percentage);
+        transform.localScale = new Vector3(1, lerpedY, 1);
+        bright.intensity = 2.1f - lerpedY;
+    }
+}
